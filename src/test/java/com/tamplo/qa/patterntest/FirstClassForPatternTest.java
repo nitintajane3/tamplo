f4b509ae1f8a094270d2eb90bed3312adc8fa718
nitintajane3 <nitintajane3@gmail.com>
2019-04-29 18:16:44 +0530

    patterns

--- src/test/java/com/tamplo/qa/patterntest/FirstClassForPatternTest.java
+++ src/test/java/com/tamplo/qa/patterntest/FirstClassForPatternTest.java
@@ -8,15 +8,18 @@ public class FirstClassForPatternTest
 	@Test
 	public void patternOne()
 	{
-		teestOne();
-		testTwo();
+		//teestOne();
+		//testTwo();
+		//testThree();
+		//testFour();
+		testFive();
 	}
 	
 	public void teestOne() 
 	{
 		for(int  i=5;i>=0;i--)
 		{
-			for(int j=5;j>=i;j--)
+			for(int j=5;j>i;j--)
 			{
 			System.out.print("*");
 			}
@@ -29,7 +32,7 @@ public class FirstClassForPatternTest
 	{
 		for(int i=0;i<=5;i++)
 		{
-			for(int j=5;j>=i;j--)
+			for(int j=5;j>i;j--)
 			{
 				System.out.print("*");
 			}
@@ -37,6 +40,170 @@ public class FirstClassForPatternTest
 			
 		}
 		
+	}
+	
+	public void testThree() 
+	{
+		/*for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println("");
+		}*/
+		
+		/*for(int p=1;p<=5;p++)
+		{
+			for(int q=5;q>=p;q--)
+			{
+				System.out.print("*");
+			}
+			System.out.println("");
+		}*/
+		
+//pattern 
+		
+		/*for(int p=5;p>=1;p--)
+		{
+			for(int z=5;z>p;z--)
+			{
+				System.out.print(" ");
+			}
+			
+			for(int q=1;q<=(2*p-1);q++)
+			{
+				
+				System.out.print("*");
+			}
+			
+			
+			System.out.println("");
+		}*/
+		/*********
+		 *******
+		  *****
+		   ***
+		    */
+		
+//pattern 	
+		for(int p=1;p<=5;p++)
+		{
+			for(int z=5;z>p;z--)
+			{
+				System.out.print(" ");
+			}
+			
+			for(int q=1;q<=(2*p-1);q++)
+			{
+				
+				System.out.print("*");
+			}
+			
+			
+			System.out.println("");
+		}
+	    /*
+	    ***
+	   *****
+	  *******
+	 *********/
+		
+		
+	}
+	
+	public void testFour() 
+	{
+		int k =1;
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=k;j++)
+			{
+				System.out.print("*");
+			}
+			k = k+2;
+			System.out.println("");
+		}
+		
+	}
+   /*
+	***
+	*****
+	*******
+	*********/
+	
+	public void testFive() 
+	{
+		int i, j, k;
+		/*for(i=1;i<=5;i++)
+		{
+		for(j=i;j<5;j++)
+		    {
+		         System.out.print(" ");
+		    }
+		for(k=1;k<(i*2);k++)
+		    {
+		         System.out.print("*");
+		    }
+		System.out.println();
+		}
+		
+		for(i=4;i>=1;i--)
+		   {
+		      for(j=5;j>i;j--)
+		         {
+		            System.out.print(" ");
+		         }
+		             for(k=1;k<(i*2);k++)
+		                 {
+		                    System.out.print("*");
+		                 }
+		               
+		    System.out.println();
+	
+		   }*/
+		
+		for(int p=1;p<=5;p++)
+		{
+			for(int z=5;z>p;z--)
+			{
+				System.out.print(" ");
+			}
+			
+			for(int q=1;q<=(2*p-1);q++)
+			{
+				
+				System.out.print("*");
+			}
+			
+			
+			System.out.println("");
+		}
+		for(int p=4;p>=1;p--)
+		{
+			for(int z=5;z>p;z--)
+			{
+				System.out.print(" ");
+			}
+			
+			for(int q=1;q<=(2*p-1);q++)
+			{
+				
+				System.out.print("*");
+			}
+			
+			
+			System.out.println("");
+		}
+	    /*
+	    ***
+	   *****
+	  *******
+	 *********
+	  *******
+	   *****
+	    ***
+	     */
 	}
 
 }
